feb7d76406f9e300d2f0a5e963ddb260d6162941
TuWebEsPoder <54472352+tuwebespoder@users.noreply.github.com>
2020-05-14 10:00:30 +0200

    Create cortarGraficos.py

---
+++ cortarGraficos.py
@@ -0,0 +1,4 @@
+//Aqui voy a generar un script en el que
+1) importaremos una grafica (datos financieros)
+2) visualizar la grafica completa
+3) exportar la grafica en pequeños tramos para poder hacer despues un gif
